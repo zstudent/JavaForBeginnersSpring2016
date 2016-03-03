9277f83f4a28a7daf2757ef26fd24df6c0b90ce1
Zaal <Zaal_Lyanov@epam.com>
2016-03-03 21:21:06 +0300

    lesson 160303 finish

--- JavaBeginner/src/lesson160303/Example03.java
+++ JavaBeginner/src/lesson160303/Example03.java
@@ -2,4 +2,20 @@ package lesson160303;
 
 public class Example03 {
 
+	public static void main(String[] args) {
+		
+		System.out.println(1 + 1);
+		System.out.println(2 * 2);
+		System.out.println((2 * 2) + (1 + 1));
+		
+		int x = 10;
+		
+		int y = 20;
+		
+		int z = x + y;
+		
+		System.out.println(z);
+		
+	}
+	
 }
